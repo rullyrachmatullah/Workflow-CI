04bbfd27e3ba0b96213630e7696ce2fa95e5b1a6
RullyRachmatullah <106619076+rullyrachmatullah@users.noreply.github.com>
2025-12-20 11:36:20 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -2,28 +2,24 @@ import pandas as pd
 import mlflow
 import mlflow.sklearn
 import os
-import dagshub
 
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, r2_score
 
-#INISIALISASI DAGSHUB & MLFLOW
-dagshub.init(
-    repo_owner="rullyrachmatullah",
-    repo_name="Eksperimen_SML_RullyRachmatullah",
-    mlflow=True
-)
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DATA_PATH = os.path.join(BASE_DIR, "med_clean.csv")
 
+# MLflow lokal (AMAN untuk CI & Docker)
+mlflow.set_tracking_uri("file:./mlruns")
 mlflow.set_experiment("insurance-exp")
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-data_path = os.path.join(BASE_DIR, "med_clean.csv")
-
 with mlflow.start_run() as run:
+    # autolog parameter, metric, model
     mlflow.sklearn.autolog()
 
-    df = pd.read_csv(data_path)
+    # load data
+    df = pd.read_csv(DATA_PATH)
 
     X = df.drop("expenses", axis=1)
     y = df["expenses"]
@@ -32,24 +28,28 @@ with mlflow.start_run() as run:
         X, y, test_size=0.2, random_state=42
     )
 
+    # model
     model = RandomForestRegressor(random_state=42)
     model.fit(X_train, y_train)
 
+    # prediction
     y_pred = model.predict(X_test)
 
-    #MANUAL METRIC (ADVANCE)
-    mlflow.log_metric("mse_manual", mean_squared_error(y_test, y_pred))
-    mlflow.log_metric("r2_manual", r2_score(y_test, y_pred))
+    mse = mean_squared_error(y_test, y_pred)
+    r2 = r2_score(y_test, y_pred)
 
-    #MODEL
-    mlflow.sklearn.log_model(model, artifact_path="model")
+    mlflow.log_metric("mse_manual", mse)
+    mlflow.log_metric("r2_manual", r2)
 
-    #KUNCI CI/CD
-    with open("run_id.txt", "w") as f:
+    mlflow.sklearn.log_model(
+        sk_model=model,
+        artifact_path="model"
+    )
+
+    with open(os.path.join(BASE_DIR, "run_id.txt"), "w") as f:
         f.write(run.info.run_id)
 
-    #TAG (nilai plus)
     mlflow.set_tag("pipeline", "ci")
     mlflow.set_tag("source", "github-actions")
 
-print("✅ Training selesai | Logged ke DagsHub | CI-ready")
+print("Training selesai | MLflow lokal | CI & Docker ready")
