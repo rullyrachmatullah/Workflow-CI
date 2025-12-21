7ff488102ef685174b32e8ebbdf482224529e8a0
RullyRachmatullah <106619076+rullyrachmatullah@users.noreply.github.com>
2025-12-21 13:40:41 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -6,6 +6,9 @@ import os
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 
+# ==============================
+# MLflow SETUP
+# ==============================
 mlflow.set_tracking_uri("file:./mlruns")
 mlflow.set_experiment("insurance-exp")
 mlflow.autolog()
@@ -13,6 +16,9 @@ mlflow.autolog()
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_PATH = os.path.join(BASE_DIR, "med_clean.csv")
 
+# ==============================
+# Load data
+# ==============================
 df = pd.read_csv(DATA_PATH)
 
 X = df.drop("expenses", axis=1)
@@ -22,14 +28,16 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-with mlflow.start_run():
-    model = RandomForestRegressor(random_state=42)
-    model.fit(X_train, y_train)
+# ==============================
+# Model (TANPA start_run)
+# ==============================
+model = RandomForestRegressor(random_state=42)
+model.fit(X_train, y_train)
 
-    mlflow.sklearn.log_model(
-        sk_model=model,
-        artifact_path="model",
-        registered_model_name="insurance-model"
-    )
+mlflow.sklearn.log_model(
+    sk_model=model,
+    artifact_path="model",
+    registered_model_name="insurance-model"
+)
 
-print("Training selesai & model terdaftar")
+print("Training selesai")
