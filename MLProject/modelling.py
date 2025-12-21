f9904b1973dcf85b12d8ecc6798b0d7ee22017cd
RullyRachmatullah <106619076+rullyrachmatullah@users.noreply.github.com>
2025-12-21 13:48:29 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -6,17 +6,11 @@ import os
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 
-# ==============================
-# AUTOLOG (WAJIB, TANPA START_RUN)
-# ==============================
 mlflow.autolog()
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_PATH = os.path.join(BASE_DIR, "med_clean.csv")
 
-# ==============================
-# Load data
-# ==============================
 df = pd.read_csv(DATA_PATH)
 
 X = df.drop("expenses", axis=1)
@@ -26,18 +20,14 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-# ==============================
-# Model
-# ==============================
 model = RandomForestRegressor(random_state=42)
 model.fit(X_train, y_train)
 
-# ==============================
-# Log model KE RUN (BUKAN REGISTRY)
-# ==============================
+# 🔑 INI KUNCI
 mlflow.sklearn.log_model(
-    sk_model=model,
-    artifact_path="model"
+    model,
+    name="model",
+    registered_model_name="insurance-model"
 )
 
-print("Training selesai")
+print("Training selesai via MLflow Project 🚀")
