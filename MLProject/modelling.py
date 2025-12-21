09bfc8b0169010884dd9d6c870f33623d577fd9b
RullyRachmatullah <106619076+rullyrachmatullah@users.noreply.github.com>
2025-12-21 13:35:00 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,21 +1,18 @@
 import pandas as pd
 import mlflow
+import mlflow.sklearn
 import os
 
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 
-# ==============================
-# AUTOLOG (WAJIB)
-# ==============================
+mlflow.set_tracking_uri("file:./mlruns")
+mlflow.set_experiment("insurance-exp")
 mlflow.autolog()
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_PATH = os.path.join(BASE_DIR, "med_clean.csv")
 
-# ==============================
-# Load data
-# ==============================
 df = pd.read_csv(DATA_PATH)
 
 X = df.drop("expenses", axis=1)
@@ -25,10 +22,14 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-# ==============================
-# Model
-# ==============================
-model = RandomForestRegressor(random_state=42)
-model.fit(X_train, y_train)
+with mlflow.start_run():
+    model = RandomForestRegressor(random_state=42)
+    model.fit(X_train, y_train)
 
-print("Training selesai via MLflow Project 🚀")
+    mlflow.sklearn.log_model(
+        sk_model=model,
+        artifact_path="model",
+        registered_model_name="insurance-model"
+    )
+
+print("Training selesai & model terdaftar")
